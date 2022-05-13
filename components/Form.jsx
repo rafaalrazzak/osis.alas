89223b048d5be1614799f0469d27efba2896b5fd
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-05-13 18:34:41 +0700

    .

--- components/Form.jsx
+++ components/Form.jsx
@@ -19,7 +19,7 @@ export default function Form() {
   };
   const [toggle, setToggle] = useState(true);
   const toggleClass = "transform translate-x-3";
-  console.log(formData)
+  console.log(formData);
   return (
     <div className="w-full lg:w-6/12">
       <div className="relative bg-white/60 filter backdrop-blur-lg shadow-md rounded-lg px-8 pt-6 pb-8 flex flex-col w-full">
@@ -142,7 +142,7 @@ export default function Form() {
           </div>
         </form>
       </div>
-      {data}
+      {formData}
     </div>
   );
 }
