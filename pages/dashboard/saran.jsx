1e8833af7eb4192369ca8da6d6e150829c378cd7
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-06-21 23:16:44 +0700

    Update saran.jsx

--- pages/dashboard/saran.jsx
+++ pages/dashboard/saran.jsx
@@ -1,14 +1,20 @@
 import Head from "next/head";
+import { useRouter } from "next/router"
 import { useState, useEffect } from "react";
 import { Card, Layout, Container, Gradient } from "@components";
 import supabase from "@/libs/supabase";
 import fetch from "@/libs/supabase/fetch";
-export default function Menfess() {
+import href from "@/data/href"
+export default function Saran() {
   const session = supabase.auth.session();
+  const { push } = useRouter()
 
   const [saran, setSaran] = useState();
   useEffect(() => {
-    if (session) {
+    if(!session){
+      push(href.signin)
+      }
+    else{
       fetch().then((x) => setSaran(x));
     }
   }, []);
