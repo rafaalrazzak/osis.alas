36860edde224dfd9855c56b966aa63af9136c68b
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2022-06-29 00:40:51 +0000

    .

--- pages/_middleware.jsx
+++ pages/_middleware.jsx
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
 export default function middleware(req) {
-  const token = req.cookies['sb-access-token'];
+  const token = req.cookies["sb-access-token"];
   const url = req.nextUrl.clone();
   url.pathname = "/";
   if (!token) return NextResponse.redirect(url);
 }
+
+export const config = {
+  matcher: "/dashboard/:path*",
+};
