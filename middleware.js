57ea520c65091682e0a95d27dd423d454411537e
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2022-07-12 13:27:50 +0000

    .

--- middleware.js
+++ middleware.js
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 export function middleware(req) {
   if (req.nextUrl.pathname.includes("/dashboard")) {
     const token = req.cookies.get("sb-access-token");
-    if (!token) return NextResponse.rewrite(new URL("/", req.url));
+    if (!token) return NextResponse.redirect(new URL("/", req.url));
   }
 }
