24c2bbca05ba88f6c224decd344e5a6d941ab6bc
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2022-07-12 11:34:18 +0000

    .

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,27 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="title" content="OSIS | SMK-AL-ASIYAH" />
-          <meta name="description" content="OSIS SMK TI AL-AL-ASIYAH" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://osis-alas.vercel.app" />
-          <meta property="og:title" content="OSIS | SMK-AL-ASIYAH" />
-          <meta property="og:description" content="OSIS SMK TI AL-AL-ASIYAH" />
-          <meta
-            property="og:image"
-            content="https://res.cloudinary.com/raf-ar/image/upload/v1655638430/thumbnail/OSIS_SMK_AL-ASIYAH.png"
-          />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://osis-alas.vercel.app" />
-          <meta property="twitter:title" content="Menfess — OSIS/MPK" />
-          <meta
-            property="twitter:description"
-            content="OSIS SMK TI AL-AL-ASIYAH"
-          />
-          <meta
-            property="twitter:image"
-            content="https://res.cloudinary.com/raf-ar/image/upload/v1655638430/thumbnail/OSIS_SMK_AL-ASIYAH.png"
-          />
           <link rel="canonical" href="https://osis-alas.vercel.app" />
           <link rel="shortcut icon" href="/static/logo-osis.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
--- pages/kontak.jsx
+++ pages/kontak.jsx
@@ -1,13 +1,13 @@
 import { SEO, Layout } from "@components";
 
-export default function Tentang() {
+export default function Kontak() {
   return (
     <>
-      <SEO title="Tentang" />
+      <SEO title="Kontak" />
       <Layout noBg>
         <div className="flex h-screen items-center justify-center">
           <h1>
-            Hello, <span className="font-semibold">tentang</span>
+            Hello, <span className="font-semibold">kontak</span>
           </h1>
         </div>
       </Layout>
