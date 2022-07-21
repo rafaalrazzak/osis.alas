8e13bf7b6e0b1cbfaf50ebd8313c2992753cc4f6
Rafa Al Razzak <94893946+rafaalrazzak@users.noreply.github.com>
2022-07-21 19:58:02 +0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: "OSIS | SMK AL-ASIYAH",
   description: "Situs Resmi OSIS SMK AL-ASIYAH",
-  siteUrl: "https://osis-alas.vercel.app",
+  siteUrl: "https://osis.matick.live",
   socialBanner: "/static/images/twitter-card.png",
   email: "contact.rafaalrazzak@gmail.com",
 };
